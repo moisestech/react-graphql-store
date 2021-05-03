f96904b40fc1102bbc524d1f6b984aedc1aabdc5
moisestech <m@moises.tech>
2021-05-03 05:03:00 -0400

    Header component

---
+++ frontend/components/Header.js
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import styles from 'styled-components';
+
+import Nav from './Nav';
+
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  background: red;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 10px solid var(--black, black);
+  }
+`;
+
+export default function Header({children}) {
+  return (
+    <HeaderStyles>
+      <div className="bar">
+        <Logo>
+          <Link href="/">
+            Sick Fits
+          </Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <Nav/>
+    </HeaderStyles>
+  );
+}
