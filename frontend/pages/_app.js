b52c957183616d4fe99c0b0294721746f3a86cf5
moisestech <m@moises.tech>
2021-05-11 04:50:42 -0400

    NProgress, Page, router, <Page> <Component pageProps/>

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,15 @@
+import NProgress from 'nprogress';
+import Page from '../components/Page';
+import router, { Router } from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.start());
+Router.events.on('routeChangeError', () => NProgress.start());
+
+// TODO: Swap with our own
+import 'nprogress/nprogress.css'
+import '../components/styles/nprogress.css'
+
 export default function MyApp({ Component, pageProps }) {
   return  <Page>
     <Component {...pageProps}/>
