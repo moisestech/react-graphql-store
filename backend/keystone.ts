053761024fc432f2bee1775014833c3a6fc3195b
moisestech <m@moises.tech>
2021-05-15 07:16:55 -0400

    createAuth, config, createSchema, Product, User, ProductImage

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,12 @@
+import { createAuth } from "@keystone-next/auth";
 import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { Product } from "./schemas/Product";
 import { User } from "./schemas/User";
+import { ProductImage } from "./schemas/ProductImage";
 import "dotenv/config";
 
 const databaseURL =
@@ -10,26 +17,45 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  // @ts-ignore
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    // TODO: Add initial roles here
   },
-  db: {
-    adapter: "mongoose",
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: `id`,
+    }),
+  })
+);
